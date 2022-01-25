bcc50db364e2d01ac950c37cb454c035948283b7
majoalba20 <mjalba458@gmail.com>
2022-01-25 14:17:39 -0500

    integrando funcionalidad

--- js/drum.js
+++ js/drum.js
@@ -3,7 +3,7 @@ let cymbal1 = document.getElementById("Component\ 13");
 let cymbal2 = document.getElementById("Component\ 16");
 let cymbal = document.getElementById("cymbal3");
 let tom = document.querySelectorAll(".inner");
-let teclas = ['f','i','o','p','k','l','a','0'];
+let teclas = ['f', 'i', 'o', 'p', 'k', 'l', 'a', '0'];
 
 ///animations
 let animationCymbal1 = () => {
@@ -19,58 +19,76 @@ let animationCymbal3 = () => {
     cymbal.style.transformOrigin = '50% 10%';
 }
 
-let stopAnimation = e =>{
+let stopAnimation = e => {
     if (e.propertyName !== 'transform') return
     e.target.style.transform = 'rotate(0deg)'
     cymbal.style.transformOrigin = '0% 0%';
 }
 
-let stopAnimationCymbals = e =>{
+let stopAnimationCymbals = e => {
     if (e.propertyName !== 'transform') return
     e.target.style.transform = 'translateY(0px)';
 }
 
-let stopAnimationTom = e =>{
+let stopAnimationTom = e => {
     if (e.propertyName !== 'transform') return
     e.target.classList.remove("active")
 }
 
 tom.forEach(tomInner => {
-    tomInner.addEventListener("transitionend",stopAnimationTom)
+    tomInner.addEventListener("transitionend", stopAnimationTom)
 })
 
-cymbal.addEventListener("transitionend",stopAnimation)
-cymbal1.addEventListener("transitionend",stopAnimationCymbals);
-cymbal2.addEventListener("transitionend",stopAnimationCymbals);
+cymbal.addEventListener("transitionend", stopAnimation)
+cymbal1.addEventListener("transitionend", stopAnimationCymbals);
+cymbal2.addEventListener("transitionend", stopAnimationCymbals);
 ///
 
-document.addEventListener("keydown",(e)=>{
+document.addEventListener("keydown", (e) => {
     let key = e.key;
     let index = teclas.indexOf(key);
-    if (index > -1) {playDrum(drum[index])}
-    switch (index){
-        case 7: animationCymbal1(); break;
-        case 5: animationCymbal2(); break;
-        case 0: animationCymbal3(); break;
+    if (index > -1) {
+        playDrum(drum[index])
     }
-    switch(index){
+    switch (index) {
+        case 7:
+            animationCymbal1();
+            break;
+        case 5:
+            animationCymbal2();
+            break;
+        case 0:
+            animationCymbal3();
+            break;
+    }
+    switch (index) {
         case 1:
         case 2:
         case 3:
         case 4:
-        case 6: tom[index].classList.add("active");break;
+        case 6:
+            tom[index].classList.add("active");
+            break;
     }
 })
 
 drum.forEach(dc => {
-    dc.addEventListener("click", ()=> playDrum(dc));
+    dc.addEventListener("click", () => playDrum(dc));
 })
 
-function playDrum(dc){
+function playDrum(dc) {
     let drumSound = document.getElementById(dc.dataset.sound)
     drumSound.currentTime = 0;
     drumSound.play();
-    if (dc == drum[7]){animationCymbal1()}
-    if (dc == drum[5]){animationCymbal2()}
-    if (dc == drum[0]){animationCymbal3()}
+    if (dc == drum[7]) {
+        animationCymbal1()
+    }
+    if (dc == drum[5]) {
+        animationCymbal2()
+    }
+    if (dc == drum[0]) {
+        animationCymbal3()
+    }
 }
+
+
--- js/script.js
+++ js/script.js
@@ -22,3 +22,5 @@ opciones.forEach(opcion => {
         })     
     })
 });
+
+
